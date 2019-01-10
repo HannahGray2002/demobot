80651afd3e8d5dafaf08664a250f0c324d4fedf8
Hannah Gray <hgra9294@shared.sydney.edu.au>
2019-01-10 16:07:23 +1100

    changed ,

--- demobot.py
+++ demobot.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/greet'. methods=['GET', 'POST'])
+@app.route('/greet', methods=['GET', 'POST'])
 def hello_world(): 
     return 'Hello, World!'
     
